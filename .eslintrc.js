535285e45d8158cd5d3d4067c9b958d54568d43e
tamaradanilova21 <roznica14@yandex.ru>
2022-10-25 11:10:59 +0400

    added BundleAnalyzer

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     "i18next/no-literal-string": ['error', { markupOnly: true }],
-    "max-len": ['error', { ignoreComments: true }]
+    "max-len": ['error', { ignoreComments: true, code: 100 }]
   },
   global: {
     '__IS_DEV__': true,
