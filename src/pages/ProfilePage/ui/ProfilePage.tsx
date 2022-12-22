b8598c19133bf87b13f361517813f71dbba09233
tamaradanilova21 <roznica14@yandex.ru>
2022-12-22 10:23:20 +0400

    added profile page

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,5 +1,11 @@
+import { profileReducer } from 'entities/Profile';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModeleLoader/DynamicModuleLoader';
+
+const reducers: ReducersList = {
+    profile: profileReducer,
+}
 
 interface ProfilePageProps {
     className?: string;
@@ -10,9 +16,14 @@ const ProfilePage = ({className}: ProfilePageProps) => {
     const { t } = useTranslation()
 
     return (
-        <div className={classNames('', {}, [className])}>
-            {t('страница профиля')}
-        </div>
+        <DynamicModuleLoader 
+            reducers={reducers} 
+            removeAfterUnmount
+        >
+            <div className={classNames('', {}, [className])}>
+                {t('страница профиля')}
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
---
+++ src/shared/const/common.ts
@@ -0,0 +1,13 @@
+export enum Currency {
+    RUB = "RUB",
+    EUR = "EUR",
+    USD = "USD",
+}
+
+export enum Country {
+    Russia = "Россия",
+    Belarus = "Беларусь",
+    Ukraine = "Украина",
+    Kazakhstan = "Кахахстан",
+    Armenia = "Армения",
+}
