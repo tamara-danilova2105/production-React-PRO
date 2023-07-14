b3d784262cfaa1a484a98bd13c20ff48588923f2
tamaradanilova21 <roznica14@yandex.ru>
2023-07-14 15:47:44 +0400

    added commit component

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,35 @@
+
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './CommentList.module.scss';
+import { useTranslation } from 'react-i18next';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentCard } from '../CommentCard/CommentCard';
+import { Comment } from '../../model/types/comment';
+
+
+interface CommentListProps {
+    className?: string;
+    comments?: Comment[];
+    isLoading: boolean;
+}
+
+export const CommentList = memo((props: CommentListProps) => {
+    const { className, comments, isLoading } = props;
+    const { t } = useTranslation('article');
+
+    return (
+        <div className={classNames(cls.CommentList, {}, [className])}>
+            {comments?.length
+                ? comments.map(comment => (
+                    <CommentCard
+                        className={cls.comment}
+                        isLoading={isLoading}
+                        comment={comment}
+                    />
+                ))
+                : <Text text={t('комментарии отсутствуют')} />
+            }
+        </div>
+    );
+});
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -4,6 +4,8 @@ import { memo } from 'react';
 import { ArticleDetails } from 'entities/Article';
 import { useParams } from 'react-router-dom';
 import cls from './ArticleDetailsPage.module.scss';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentList } from 'entities/Comment';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -25,6 +27,19 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     return (
         <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
             <ArticleDetails id={id} />
+            <Text className={cls.commentTitle} title={t('комментарии')} />
+            <CommentList isLoading={true} comments={[
+                {
+                    id: '1',
+                    text: 'comment 1',
+                    user: {id: '1', username: 'admin', avatar: 'https://img5.goodfon.ru/original/6016x4016/4/de/pink-background-pose-charming-woman-brown-eyes-bright-manicu.jpg'}
+                },
+                {
+                    id: '2',
+                    text: 'comment 2',
+                    user: {id: '1', username: 'admin', avatar: 'https://img5.goodfon.ru/original/6016x4016/4/de/pink-background-pose-charming-woman-brown-eyes-bright-manicu.jpg'}
+                }
+            ]} />
         </div>
     );
 };
