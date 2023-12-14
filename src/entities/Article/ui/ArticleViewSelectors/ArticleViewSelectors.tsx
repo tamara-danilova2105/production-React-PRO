1f9f6af65ccc4013bddcbcf7e57b4c8c4f055f35
tamaradanilova21 <roznica14@yandex.ru>
2023-12-14 16:39:55 +0400

    added fetch aticles, view type

---
+++ src/entities/Article/ui/ArticleViewSelectors/ArticleViewSelectors.tsx
@@ -0,0 +1,51 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import cls from './ArticleViewSelectors.module.scss';
+import { ArticleView } from '../../model/types/article';
+import ListIcon from 'shared/assets/icons/list-24-24.svg';
+import TiledIcon from 'shared/assets/icons/tiled-24-24.svg';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+
+interface ArticleViewSelectorsProps {
+    className?: string;
+    view: ArticleView,
+    onViewClick?: (view: ArticleView) => void,
+}
+
+const viewTypes = [
+    {
+        view: ArticleView.SMALL,
+        icon: TiledIcon,
+    },
+    {
+        view: ArticleView.BIG,
+        icon: ListIcon,
+    }
+]
+
+export const ArticleViewSelectors = memo((props: ArticleViewSelectorsProps) => {
+    const { className, view, onViewClick } = props;
+
+    const onClick = (newView: ArticleView) => () => {
+        onViewClick?.(newView);
+    };
+
+    return (
+        <div className={classNames(cls.ArticleViewSelectors, {}, [className])}>
+            {
+                viewTypes.map(viewTupe => (
+                    <Button
+                        theme={ThemeButton.CLEAR}
+                        onClick={onClick(viewTupe.view)}
+                    >
+                        <Icon
+                            Svg={viewTupe.icon}
+                            className={classNames('', { [cls.notSelected]: viewTupe.view === view })}
+                        />
+                    </Button>
+                ))
+            }
+        </div>
+    );
+});
--- src/shared/const/localStorage.ts
+++ src/shared/const/localStorage.ts
@@ -1,1 +1,2 @@
 export const USER_LOCALSTORAGE_KEY = 'user';
+export const ARTILES_VIEW_LOCALSTORAGE_KEY = 'articles_view';
