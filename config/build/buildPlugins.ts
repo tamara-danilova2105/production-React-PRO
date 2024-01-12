f0404aa03be357ca1eacb2b58773c6b58a745a30
tamaradanilova21 <roznica14@yandex.ru>
2024-01-12 13:19:08 +0400

    build config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
+import CopyPlugin from 'copy-webpack-plugin';
 
 export function buildPlugins({
     paths, isDev, apiUrl, project,
@@ -20,6 +21,11 @@ export function buildPlugins({
             __IS_DEV__: JSON.stringify(isDev),
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project),
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: paths.locales, to: paths.buildLocales}
+            ],
         }),
     ];
 
